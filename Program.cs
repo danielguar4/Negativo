ea07ce127f414f1d3bae04f7ea1fc98df3437cb6
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 19:25:47 -0300

    Projeto Concluido

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+int Numero;
+
+Console.Write("Digite um numero...: ");
+Numero = Convert.ToInt32(Console.ReadLine());
+
+if (Numero < 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Você digitou um número negativo.");
+    }
+
+Console.ResetColor();
